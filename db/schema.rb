b61b3f118cdc3331180e403c424a6207a7a4ce26
Harpal Singh <oneharpal@gmail.com>
2024-10-27 22:37:06 +0530

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_170337) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_170637) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "messages", "rooms"
 end
