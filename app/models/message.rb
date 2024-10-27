29c9d00c0ed6f5b5fb0de885593c4cc253891eb0
Harpal Singh <oneharpal@gmail.com>
2024-10-28 03:45:08 +0530

    broadcast message crud to room

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :room
-  after_create_commit -> { broadcast_append_to room }
-  after_destroy_commit -> { broadcast_remove_to room }
+  broadcasts_to :room
 end
